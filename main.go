9c69324120d90d643b1aabdd35c5c4c218b80c1a
SmallRoomLabs <git@smallroomlabs.com>
2019-03-09 11:00:47 +0100

    Make project successful again

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	db := SetupDB()
 	server := service.NewServer(db)
 	http.HandleFunc("/", server.ServeHTTP)
-	http.apa.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func SetupDB() *service.Database {
